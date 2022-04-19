337e7e93b5bae9a84f77ebe525c911337947b156
Kuber <kuber.22info@gmail.com>
2022-04-19 14:39:57 +0530

    first commit

---
+++ G.java
@@ -0,0 +1,10 @@
+class G{
+    public static void main(String[] args){
+        int w =  64378;
+        int f =  3874893;
+        float g =  4873.87897F;
+
+        System.out.println(g);
+        
+    }
+}
